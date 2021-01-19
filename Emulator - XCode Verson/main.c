b30fcb724cba9c949f497bc6dc913020da622726
Eric Diskin <ericdiskin26@gmail.com>
2021-01-18 22:38:08 -0500

    Initial Commit

---
+++ Emulator - XCode Verson/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Emulator - XCode Verson
+//
+//  Created by Eric Diskin on 1/18/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
